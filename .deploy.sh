44fc1ea865f21d345d2acd92c418f3f4cc7f27de
Niels Grewe <niels.grewe@halbordnung.de>
2017-05-26 23:00:29 +0200

    Remove debugging, clean up

--- .deploy.sh
+++ .deploy.sh
@@ -1,12 +1,12 @@
-#!/bin/sh -xe
-if [[ -n "${TRAVIS_TAG}" ]]; then
+#!/bin/sh -e
+if [ -n "${TRAVIS_TAG}" ]; then
   docker tag ngrewe/build.rs:latest ${TRAVIS_TAG}	
 fi
 
-docker login -e "${DOCKER_EMAIL}" -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 trap 'docker logout' EXIT
 docker push ngrewe/build.rs:latest
 
-if [[ -n "${TRAVIS_TAG}" ]]; then
+if [ -n "${TRAVIS_TAG}" ]; then
   docker push ngrewe/build.rs:${TRAVIS_TAG}
 fi 
